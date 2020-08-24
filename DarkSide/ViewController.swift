1406b1132b7f50e415488934a294bfd2b1fd2ee2
Kintija <kintija.p@gmail.com>
2020-08-24 10:55:50 +0300

    dark side on/off button

--- DarkSide/ViewController.swift
+++ DarkSide/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var darkModeButton: UIButton!
+    
+    var isOn = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad")
+        //darkModeButton.layer.cornerRadius = 10
     }
-
-
+    @IBAction func darkModeTapped(_ sender: Any) {
+        if isOn {
+            print("Dark mode on")
+            self.isOn = false
+            self.darkModeButton.setTitle("Dark Mode on", for: .normal)
+            self.darkModeButton.setTitleColor(UIColor.white, for: .normal)
+            self.view.backgroundColor = UIColor.black
+        }else{
+            print("Dark mode off")
+            self.isOn = true
+            self.darkModeButton.setTitle("Dark Mode off", for: .normal)
+            self.darkModeButton.setTitleColor(UIColor.black, for: .normal)
+            self.view.backgroundColor = UIColor.white
+        }
+        
+        
+    }
+    
 }
-
